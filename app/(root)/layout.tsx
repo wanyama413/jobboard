1b890263c33883606cf132e3375d03073888ad62
Sammy Mathias Wanyama <99073324+wanyama413@users.noreply.github.com>
2024-01-28 00:57:10 +0300

    Camel

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import type { Metadata } from "next";
 import "../globals.css";
 import React from "react";
@@ -20,16 +21,16 @@ import {
   TrendingUpIcon,
 } from "lucide-react";
 
-import { Inter, Space_Grotesk } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 export const metadata: Metadata = {
   title: "Jobs Reservoir",
   description:
     "Discover your next career opportunity with our dynamic job board web application. Explore diverse job listings, connect with top employers, and take the next step in your professional journey. Streamlined and user-friendly, our platform is designed to match you with the perfect job effortlessly. Elevate your career with our innovative job board – where opportunities meet aspirations.",
 };
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+// const inter = Inter({
+//   subsets: ["latin"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+// });
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
